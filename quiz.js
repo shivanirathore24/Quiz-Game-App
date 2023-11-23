54986e493996471815568881c23eb9a9cd59b03e
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-11-23 20:10:55 +0530

    Displaying the Question

--- quiz.js
+++ quiz.js
@@ -1,9 +1,18 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "Three",
+  answers: ["Two", "Three ", "Four", "Five"],
+  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
+
+//destructing properties from Object
+const { correctAnswer, options, question } = questionObj;
+
+//accessing all the elements
+const questionEl = document.getElementById("question");
+const optionEl = document.getElementById("options");
+const scoreEl = document.getElementById("score");
+
+//setting question the text-content
+questionEl.textContent = question;
