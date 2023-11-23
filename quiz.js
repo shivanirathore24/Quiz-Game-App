b64d8db5fbc4e7a568f5d3d06aaebcefec160530
shivanirathore24 <shivani.rathore2409@gmail.com>
2023-11-23 20:33:39 +0530

    Display & select the Correct Option

--- quiz.js
+++ quiz.js
@@ -2,12 +2,16 @@ const questionObj = {
   category: "Food & Drink",
   id: "qa-1",
   correctAnswer: "Three",
-  answers: ["Two", "Three ", "Four", "Five"],
+  options: ["Two", "Three", "Four", "Five"],
   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
 };
 
+/** Manipulating DOM ***/
+
 //destructing properties from Object
 const { correctAnswer, options, question } = questionObj;
+
+let score = 0;
 
 //accessing all the elements
 const questionEl = document.getElementById("question");
@@ -16,3 +20,26 @@ const scoreEl = document.getElementById("score");
 
 //setting question the text-content
 questionEl.textContent = question;
+
+/*** Populating the options div with the buttons ***/
+options.forEach((opt) => {
+  //creating button & appending it to the option div
+  const btn = document.createElement("button");
+  btn.textContent = opt;
+  optionEl.appendChild(btn);
+
+  //event handling on the button
+  btn.addEventListener("click", () => {
+    if (opt === correctAnswer) {
+      score++;
+    } else {
+      score = score - 0.25;
+    }
+    //console.log(score);
+    scoreEl.textContent = `Score: ${score}`;
+
+    //after attempting quetion, options shouldn't show and display Quiz Completed
+    questionEl.textContent = "Quiz Completed !!";
+    optionEl.textContent = "";
+  });
+});
